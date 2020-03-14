33eb4399a57693a46feb04a90b5a681f5db09670
yikaifeng <60769313+yikaifeng@users.noreply.github.com>
2020-03-14 12:34:20 +0300

    Create test5.js

---
+++ test5.js
@@ -0,0 +1,19 @@
+const LIB1 = "Тест1"；
+const LIB2 = "Тест2"；
+
+const NAME = "Название"；
+const RESULT = "Результат";
+var res = "Результат:";
+
+function test() {
+	
+	var en = entry();
+	var lib = libByName(LIB2);
+	var links = lib.linksTo(en);
+	
+	for (i =0; i < links.length; i++){
+		res = res + "\n" + links[i].field("NAME");
+	}
+	
+	en.set(RESULT, res);
+}
