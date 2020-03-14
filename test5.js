7c965f3a765b66e0e054036c27ed182ad47a6591
yikaifeng <60769313+yikaifeng@users.noreply.github.com>
2020-03-14 12:43:57 +0300

    Update test5.js

--- test5.js
+++ test5.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const LIB1 = "Тест1"；
-const LIB2 = "Тест2"；
+const LIB_1 = "Тест1"；
+const LIB_2 = "Тест2"；
 
 const NAME = "Название"；
 const RESULT = "Результат";
@@ -10,7 +10,7 @@ var res = "Результат:";
 function test3() {
 	
 	var en = entry();
-	var lib = libByName(LIB2);
+	var lib = libByName(LIB_2);
 	var links = lib.linksTo(en);
 	
 	for (i =0; i < links.length; i++){
