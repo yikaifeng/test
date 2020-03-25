c6dccc902415d16643fffff56fd8c88501240909
yikaifeng <60769313+yikaifeng@users.noreply.github.com>
2020-03-25 15:49:37 +0300

    Update common.js

--- common.js
+++ common.js
@@ -16,191 +16,271 @@
 //======================================================
 const Edit = {};
 	
-	//Получит иконку из начала строки
-	Edit.getIcon = function(sSource) {
+	//--------------------------------------------------
+	//Получить иконку из начала строки
+	Edit.getIcon = function (sSource) {
+		
+		//Проверяем, передана ли строка
 		if (typeof(sSource) != String) {
-			return null;
-		} else {
-			var separator = " ";
-			sSource.toString();
-			return sSource.split(separator,1)[0];
+			var sMessage = "\nОшибка Edit.getIcon(sSource):\nпереданное значение не является строкой";
+			throw new Error(sMessage);
 		}
+		
+		//Проверяем, не пустая ли строка
+		if (sSource.length == 0) {
+			var sMessage = "\nОшибка Edit.getIcon(sSource):\nпустая строка";
+			throw new Error(sMessage);
+		}
+		
+		//Проверяем, есть ли хотя бы 1 пробел
+		if (sSource.indexOf(" ") == -1) {
+			var sMessage = "\nОшибка Edit.getIcon(sSource):\nотсутствует пробел в строке";
+			throw new Error(sMessage);
+		}
+		
+		var separator = " ";
+		var sIcon = sSource.split(separator,1)[0];
+		
+		//Проверяем, не пустая ли строка в результате
+		if (sIcon.length == 0) {
+			var sMessage = "\nОшибка Edit.getIcon(sSource):\nиконка в строке отсутствует";
+			throw new Error(sMessage);
+		}
+		
+		return sIcon;
+		
 	}
 	
+	//--------------------------------------------------
 	//Получить строку без иконки в начале строки
 	Edit.getText = function (sSource) {	
+	
+		//Проверяем, передана ли строка
 		if (typeof(sSource) != String) {
-			return null;
-		} else {
-			var separator = " ";
-			sSource.toString();
-			var strIcon = strSource.split(separator,1)[0];
-			var strName = strSource.slice(strIcon.length);
-			return strName.trim();
+			var sMessage = "\nОшибка Edit.getText(sSource):\nпереданное значение не является строкой";
+			throw new Error(sMessage);
 		}
+		
+		//Проверяем, не пустая ли строка
+		if (sSource.length == 0) {
+			var sMessage = "\nОшибка Edit.getText(sSource):\nпустая строка";
+			throw new Error(sMessage);
+		}
+		
+		//Проверяем, есть ли хотя бы 1 пробел
+		if (sSource.indexOf(" ") == -1) {
+			var sMessage = "\nОшибка Edit.getText(sSource):\nотсутствует пробел в строке";
+			throw new Error(sMessage);
+		}
+		
+		var separator = " ";
+		var sIcon = sSource.split(separator,1)[0];
+		var sName = sSource.slice(sIcon.length);
+		sName.trim()
+		
+		//Проверяем, не пустая ли строка в результате
+		if (sName.length == 0) {
+			var sMessage = "\nОшибка Edit.getText(sSource):\nназвание в строке отсутствует";
+			throw new Error(sMessage);
+		}
+		
+		return sName;
+
 	}
 	
+	//--------------------------------------------------
 	//Получить деньги
 	Edit.getMoney = function (nSum, sCurrency) {
+		
+		//Проверяем, передана ли число
 		if (typeof(nSum) != Number) {
-			return null;
-		} else {
-			var sign = "";
+			var sMessage = "\nОшибка Edit.getMoney(nSum, sCurrency):\nnSum не является числом";
+			throw new Error(sMessage);
+		}
+		
+		nSum = nSum.toFixed(2);
+		
+		var sign = "";
+		if (nSum < 0) {
+			sign = "-";
+			nSum = Math.abs(nSum);
 			nSum = nSum.toFixed(2);
-			if (nSum < 0) {
-				sign = "-";
-				nSum = Math.abs(nSum);
-				nSum = nSum.toFixed(2);
-			}
-			nSum += "";
-			nSum = new Array(4 - nSum.length % 3).join("U") + nSum;
-			nSum = nSum.replace(/([0-9U]{3})/g, "$1 ").replace(/U/g, "");
-			nSum = sign + nSum;
-			nSum = nSum.trim();
-			if (typeof(sCurrency) == String && sCurrency != undefined) {
-				return nSum + " " + sCurrency;
-			} else {
-				return nSum;
-			}		
-		}	
+		}
+		
+		nSum += "";
+		nSum = new Array(4 - nSum.length % 3).join("U") + nSum;
+		nSum = nSum.replace(/([0-9U]{3})/g, "$1 ").replace(/U/g, "");
+		nSum = sign + nSum;
+		nSum = nSum.trim();
+		
+		if (sCurrency != undefined && typeof(sCurrency) == String) {
+			return nSum + " " + sCurrency;
+		} else {
+			return nSum;
+		}		
+			
 	}
 	
+	//--------------------------------------------------
 	//Название месяца на русском по его номеру
 	Edit.getMonthName = function (nMonthNumber) {
 		
+		//Проверяем, передана ли число
 		if (typeof(nMonthNumber) != Number) {
-			return null;
-		} else {
-			switch (nMonthNumber) {
-				case 1:
-					return "январь";
-					break;
-				case 2:
-					return "февраль";
-					break;
-				case 3:
-					return "март";
-					break;
-				case 4:
-					return "апрель";
-					break;
-				case 5:
-					return "май";
-					break;
-				case 6:
-					return "июнь";
-					break;
-				case 7:
-					return "июль";
-					break;
-				case 8:
-					return "август";
-					break;
-				case 9:
-					return "сентябрь";
-					break;
-				case 10:
-					return "октябрь";
-					break;
-				case 11:
-					return "ноябрь";
-					break;
-				case 12:
-					return "декабрь";
-					break;
-				default:
-					return null;
-					break;
-			}
+			var sMessage = "\nОшибка Edit.getMonthName(nMonthNumber):\nnMonthNumber не является числом";
+			throw new Error(sMessage);
 		}
 		
+		var arrMonthsNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+		var arrMonthsNames = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+		
+		
+		//Проверяем, передана ли число от 1 до 12
+		if (!arrMonthsNumbers.includes(nMonthNumber)) {
+			var sMessage = "\nОшибка Edit.getMonthName(nMonthNumber):\nnMonthNumber не является номером месяца";
+			throw new Error(sMessage);
+		}		
+		
+		return arrMonthsNames[nMonthNumber-1];		
 		
 	}
-	
+
+	//--------------------------------------------------
 	//Сдвиг даты на определенный интервал
 	Edit.shiftDate = function (dDate, nInterval, sUnit, bForward) {
 		
-		if (typeof(dDate) != Number || 
-			typeof(nInterval) != Number || nInterval < 0 ||
-			typeof(sUnit) != String || sUnit != "d" || sUnit != "w" || sUnit != "m" || sUnit != "y") {
+		//Проверяем, передана ли дата в виде числа
+		if (typeof(dDate) != Number) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\ndDate не является числом";
+			throw new Error(sMessage);
+		}
+
+		//Проверяем, переданан ли интервал в виде числа
+		if (typeof(nInterval) != Number) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nnInterval не является числом";
+			throw new Error(sMessage);
+		}
+
+		//Проверяем, переданан интервал больше ли нуля
+		if (nInterval < 0) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nnInterval < 0";
+			throw new Error(sMessage);
+		}		
+		
+		//Проверяем, переданан единицы сдвига являются ли строкой
+		if (typeof(sUnit) != String) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nsUnit не является строкой";
+			throw new Error(sMessage);
+		}		
+		
+		//Проверяем, переданан единицы корректны ли
+		var arrUnits = ["d", "w", "m", "y"];
+		if (!arrUnits.includes(sUnit)) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nsUnit не является d/w/m/y";
+			throw new Error(sMessage);
+		}	
+		
+		//Проверяем, направление логическая ли величина
+		if (typeof(bForward) != Boolean && bForward != undefined) {
+			var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nbForward не логическая величина";
+			throw new Error(sMessage);
+		}	
+		
+		var sign = 1;
+		if (!bForward) {sign = -1;}
 				
-				return null;
+			switch (sUnit) {
 				
-			} else {
-				var sign = 1;
-				if (!bForward) {sign = -1;}
-				
-				switch (sUnit) {
+				case "d":
+					dDate.setDate(dDate.getDate() + sign*nInterval);
+					return dDate;
+					break;
 					
-					case "d":
-						dDate.setDate(dDate.getDate() + sign*nInterval);
-						return dDate;
-						break;
+				case "w":
+					dDate.setDate(dDate.getDate() + sign*nInterval*7);
+					return dDate;
+					break;
+					
+				case "m":
+					//сохраняем дату и устанавливаем 1 число месяца
+					var nOriginalDate = dDate.getDate();
+					dDate.setDate(1);
 						
-					case "w":
-						dDate.setDate(dDate.getDate() + sign*nInterval*7);
-						return dDate;
-						break;
+					//Прибавляем нужное число месяцев
+					dDate.setMonth(dDate.getMonth() + sign*nInterval);
+					var nNewMonth = dDate.getMonth();
+					
+					//Восстанавливаем дату без перескока месяца
+					dDate.setDate(nOriginalDate);
 						
-					case "m":
-						//сохраняем дату и устанавливаем 1 число месяца
-						var nOriginalDate = dDate.getDate();
-						dDate.setDate(1);
-							
-						//Прибавляем нужное число месяцев
-						dDate.setMonth(dDate.getMonth() + sign*nInterval);
-						var nNewMonth = dDate.getMonth();
-						
-						//Восстанавливаем дату без перескока месяца
-						dDate.setDate(nOriginalDate);
-							
-						var loop = 10;
-						while (nNewMonth != dDate.getMonth() && loop>0) {
-							dDate.setDate(dDate.getDate()-1);
-							loop = loop - 1;
-						}
-						return dDate;
-						break;
-						
-					case "y":
-						dDate.setFullYear(dDate.getFullYear() + sign*nInterval);
-						return dDate;
-						break;
-						
-					default :
-						return null;
-						break;
-				}
-				
-			}
+					var loop = 10;
+					while (nNewMonth != dDate.getMonth() && loop>=0) {
+						dDate.setDate(dDate.getDate()-1);
+						loop = loop - 1;
+					}
+					//Если прерывание по количеству циклов
+					if (loop <= 0) {
+						var sMessage = "\nОшибка Edit.shiftDate(dDate, nInterval, sUnit, bForward):\nсдвиг месяца прерван по превышению 10 циклов";
+						throw new Error(sMessage);
+					}
+					return dDate;
+					break;
+					
+				case "y":
+					dDate.setFullYear(dDate.getFullYear() + sign*nInterval);
+					return dDate;
+					break;
 			
+			}
+		
 	}
 	
+	//--------------------------------------------------
 	//Остаток дней
 	Edit.daysLeft = function (dDate, dTarget, nRound) {
 	
+		//Проверяем, передана ли дата в виде числа
 		if (typeof(dDate) != Number) {
-			
-			return null;
-			
-		} else {
-			
-			//Если не задана конечная дата, то сегодня
-			if (dTarget == undefined || typeof(dTarget) != Number) {
-				dTarget = new Date();
-			}
-			if (nRound == undefined || nRound < 0 || typeof(nRound) != Number) {
-				nRound = 0;
-			}
-			
-			//Разница в днях с сейчас
-			var dDif = (dDate - dTarget)/(1000*3600*24);
-				
-			//Округляем
-			return dDif.toFixed(nRound);
-						
+			var sMessage = "\nОшибка Edit.daysLeft(dDate, dTarget, nRound):\ndDate не является числом";
+			throw new Error(sMessage);
 		}
 		
+		//Проверяем, передана ли дата dTarget в виде числа
+		if (typeof(dTarget) != Number && dTarget != undefined) {
+			var sMessage = "\nОшибка Edit.daysLeft(dDate, dTarget, nRound):\ndTarget не является числом";
+			throw new Error(sMessage);
+		}
+		
+		//Проверяем, передана ли количество знаков округления nRound в виде числа
+		if (typeof(nRound) != Number && nRound != undefined) {
+			var sMessage = "\nОшибка Edit.daysLeft(dDate, dTarget, nRound):\nnRound не является числом";
+			throw new Error(sMessage);
+		}
+		
+		//Проверяем, nRound больше ли 0
+		if (nRound < 0) {
+			var sMessage = "\nОшибка Edit.daysLeft(dDate, dTarget, nRound):\nnRound < 0";
+			throw new Error(sMessage);
+		}
+		
+		//Проверяем, nRound целое ли
+		var test = nRound;
+		test.toFixed(0);
+		if (nRound != test) {
+			var sMessage = "\nОшибка Edit.daysLeft(dDate, dTarget, nRound):\nnRound не целое";
+			throw new Error(sMessage);
+		}
+	
+		//Если не задана конечная дата, то сегодня
+		if (dTarget == undefined) {dTarget = new Date();}
+		if (nRound == undefined) {nRound = 0;}
+			
+		//Разница в днях с сейчас
+		var dDif = (dDate - dTarget)/(1000*3600*24);
+				
+		//Округляем
+		return dDif.toFixed(nRound);
+						
 	}
 	
 Object.freeze(Edit);
